da7d267cbf3dc860dc78b1e43c44136d9c165e2f
Eyal Betzalel <eyalbetzalel@dsief01.local.biu.ac.il>
2022-01-02 16:08:34 +0200

    bla

--- wgan_gp.py
+++ wgan_gp.py
@@ -19,7 +19,7 @@ import torch
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=20, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
@@ -95,10 +95,10 @@ if cuda:
     discriminator.cuda()
 
 # Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
+os.makedirs("./data/mnist", exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
     datasets.MNIST(
-        "../../data/mnist",
+        "./data/mnist",
         train=True,
         download=True,
         transform=transforms.Compose(
