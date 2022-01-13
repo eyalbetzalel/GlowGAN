0bda58dfb645f35af5a72019ebfd857926fc09a8
eyalbetzalel <eyalbatzul@gmail.com>
2022-01-13 14:45:23 +0200

    js similarity add blank functions

---
+++ GlowGan/realnvp_gan/js_test.py
@@ -0,0 +1,16 @@
+def measure_prob_on_imagegpt_samples(generator, imagegpt_class, imgs):
+    p_imagegpt, q_realnvp= 0, 0
+    return p_imagegpt, q_realnvp
+
+def calc_js_div(p_imagegpt, p_realnvp, q_imagegpt, q_realnvp):
+    js_div=0
+    return js_div
+
+def calc_gradient(net):
+    grad=0
+    return grad
+
+def calc_cosine_similarity(vec1, vec2):
+    cos_sim=0
+    return cos_sim
+
